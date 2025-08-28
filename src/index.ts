c9097fdd24246843142958cd32b66e7f039ec61a
Arnab Golder <arnab.g@webbixel.com>
2025-08-28 11:21:44 +0530

    bug fix v0.0.1.3

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
 import express, { Request, Response } from "express";
-
+import client from "prom-client";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Create a Registry to register the metrics
+const register = new client.Registry();
+// Default metrics like CPU, memory, event loop lag, etc.
+client.collectDefaultMetrics({ register });
+
+
+
+// Custom counter metric
+const httpRequests = new client.Counter({
+  name: "http_requests_total",
+  help: "Total number of HTTP requests",
+  labelNames: ["method", "route", "status"],
+});
+
+register.registerMetric(httpRequests);
+
+// custom middleware to count HTTP requests
+app.use((req: Request, res: Response, next) => {
+  httpRequests.inc({ method: req.method, route: req.path, status: res.statusCode });
+  next();
+});
+
 // GET /hello
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello from nodeApp");
+  res.send("Hello from nodeApp from vps");
+
 });
 
 // GET /health
@@ -13,6 +36,6 @@ app.get("/health", (req: Request, res: Response) => {
   res.json({ status: "OK" });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT , () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
